f46668b14be5c2eca5c4626e26b2c9bf0975a1bf
julia-ms <mombachdasilva@gmail.com>
2023-04-26 18:52:20 -0300

    day1

---
+++ day1/class.py
@@ -0,0 +1,20 @@
+#comments
+
+print("hello world!")
+
+print("hello world!\nhello world!")
+
+print("hello" + " " + "julia")
+
+input("what is your name?")
+
+print("Hi " + input("your name:"))
+
+name = input("whats your name?")
+print(name)
+
+length = len(name)
+print(length)
+
+name = "julia"
+print(name)
