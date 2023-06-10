303df5557d72795091de98c34fa43586f1f3d86f
julia-ms <mombachdasilva@gmail.com>
2023-06-10 17:38:17 -0300

    Create class.py

---
+++ day2/class.py
@@ -0,0 +1,45 @@
+#string
+print("hello"[4])
+#o
+
+print("123" + "456")
+#123456
+
+#integer
+print(123+456)
+#579
+
+
+#float
+pi = 3.141592
+print(pi)
+
+#boolean -> true/false
+
+#------------------------------------
+
+num_char = len(input("whats your name?"))
+
+new_numchar = str(num_char)
+
+print("your name has " + new_numchar + " characters")
+
+#------------------------------------------
+
+#MATH
+
+# 2 + 3 = 5
+# 2 - 3 = -1
+# 2 * 3 = 6
+# 2 / 3 = 0,6666
+# 2 ** 3 = 8
+
+# round(8/3) -> arredonda pra int
+# round(8/3, 2) -> arredonda com duas casa decimais
+# 8//3 -> transforma em int (resposta fica 2)xzSAZGUGVUSDBNVJBKJDBUBJV
+
+score = 0
+height = 1.8
+isWinning = True
+
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
